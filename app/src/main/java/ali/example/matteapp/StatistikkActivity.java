bbbe50364b9a2646aeaebc2b6d723651daf8bf56
Adnan <adnag>
2020-09-05 15:35:56 +0200

    Added nnn

--- app/src/main/java/ali/example/matteapp/StatistikkActivity.java
+++ app/src/main/java/ali/example/matteapp/StatistikkActivity.java
@@ -10,5 +10,8 @@ public class StatistikkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistikk);
+
+        System.out.println("vcvcv");
+
     }
 }
